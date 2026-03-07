c5c8ed6c9ef9b8185bcd22495a3cc4c7c084142e
sperrodigital <oleg.kontsograda@sperrodigital.com>
2026-03-07 08:09:56 -0500

    adding pickup resources

---
+++ Scenes/wood.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		# TODO: add item to player inventory here
+		queue_free()
